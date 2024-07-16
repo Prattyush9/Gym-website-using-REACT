a30110d2c36d6a5d50312a29fe1b56e0f8ba2c29
Prattyush9 <159418848+Prattyush9@users.noreply.github.com>
2024-07-16 13:32:28 +0530

    Add files via upload

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+        Designed And Developed By Prattyush Ranjan Nayak
+    </footer>
+  )
+}
+
+export default Footer
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <header>
+      <p>IRON BODY FITNESS</p>
+    </header>
+  );
+};
+
+export default Navbar;
